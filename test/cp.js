78d09493b412afdb29a9008cc3ff4096136a7b00
counterm <64436097@qq.com>
2021-01-25 17:43:04 +0800

    修正路径

--- test/cp.js
+++ test/cp.js
@@ -7,3 +7,15 @@ var destPath = path.join(__dirname, "../demo", "index.html");
 var readStream = fs.createReadStream(sourceFile);
 var writeStream = fs.createWriteStream(destPath);
 readStream.pipe(writeStream);
+
+var replace = require("replace");
+
+readStream.on('end', () => {
+    replace({
+        regex: "../src/home.js",
+        replacement: "./table_buster.min.js",
+        paths: ['./demo/index.html'],
+        recursive: false,
+        silent: true,
+    });
+})
